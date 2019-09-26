f5710d6a3c33bb71e101923c01769811bd95cf39
Demmy <demolaabdulai@live.com>
2019-09-27 00:01:17 +0200

    finshed Member Page

--- js/member.js
+++ js/member.js
@@ -3,11 +3,18 @@ var json = {
             "Name": "Vaughan Richard",
             "Location": "  Lagos, Nigeria",
             "imgPath": "img/members/JRBig.jpg",
-            "Nickname": "Nickname : JR",
-            "DateOfBirth": "Date of : 13th August",
-            "Occupation": "Occupation : Politician"
+            "Nickname": "Nickname : Campos",
+            "DateOfBirth": "Date of : 18th June",
+            "Occupation": "Occupation : Media Practitioner"
         },
-
+        {
+            "Name": "Buariu A. Ogundimu",
+            "Location": "  London, UK",
+            "imgPath": "img/members/BayBig.jpg",
+            "Nickname": "Nickname : Alhaji Solo",
+            "DateOfBirth": "Date of : 13th Feb",
+            "Occupation": "Occupation : Project Manager"
+        },
         {
             "Name": "Demola Abdulai",
             "Location": "Copenhagen, Denmark",
@@ -15,6 +22,335 @@ var json = {
             "Nickname": "Nickname : Demmy",
             "DateOfBirth": "Date of : 13th August",
             "Occupation": "Occupation : IT Developer"
+        },
+        {
+            "Name": "Olawale A. Aileru",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/waleBig.jfif",
+            "Nickname": "Nickname : Mark Fish",
+            "DateOfBirth": "Date of : 13th Feb",
+            "Occupation": "Occupation : Business Man"
+        },
+        {
+            "Name": "Jamiu Eko",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/jamiu_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : ",
+            "Occupation": "Occupation : "
+        },
+        {
+            "Name": "Adedeji Martin",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/osho_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : ",
+            "Occupation": "Occupation : "
+        },
+        {
+            "Name": "Ebenezer Idowu",
+            "Location": "  United Kingdom",
+            "imgPath": "img/members/EbenezerIdow_big-min.JPG",
+            "Nickname": "Nickname : Obey",
+            "DateOfBirth": "Date of : 1st September",
+            "Occupation": "Occupation : Mental health Nurse"
+        },
+        {
+            "Name": "Ray Azagba",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/ray_big.jpg",
+            "Nickname": "Nickname : Shadow",
+            "DateOfBirth": "Date of : ",
+            "Occupation": "Occupation : "
+        },
+        {
+            "Name": "Sodiq Reis",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/reis_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : ",
+            "Occupation": "Occupation : "
+        },
+        {
+            "Name": "Ajayibembe Muyiwa",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/avatar.png",
+            "Nickname": "Nickname : bembe",
+            "DateOfBirth": "Date of : April 5",
+            "Occupation": "Occupation : Civil servant"
+        },
+        {
+            "Name": "Sadiku Ibitayo",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/sadiku_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 2nd August",
+            "Occupation": "Occupation : Legal practitioner"
+        },
+        {
+            "Name": "Albert Adesope L. Adeniyi",
+            "Location": "  Sultanate of Oman",
+            "imgPath": "img/members/avatar.png",
+            "Nickname": "Nickname : Roberto Baggio/DelPiero",
+            "DateOfBirth": "Date of : 30th August",
+            "Occupation": "Occupation : Nurse Administrator"
+        },
+        {
+            "Name": "Olowookere Joseph",
+            "Location": "  South Africa",
+            "imgPath": "img/members/JosephOlowokere_big.JPG",
+            "Nickname": "Nickname : SP",
+            "DateOfBirth": "Date of : 7th April",
+            "Occupation": "Occupation : Hospitality /Restaurant Operator"
+        },
+        {
+            "Name": "Sho-silva Shokunbi",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/Shokunbi_big.jpg",
+            "Nickname": "Nickname : Sho",
+            "DateOfBirth": "Date of : 5th January",
+            "Occupation": "Occupation : Information Technology personnel"
+        },
+        {
+            "Name": "Moses Joseph-Soussoukpor OCD",
+            "Location": "  Abuja, Nigeria",
+            "imgPath": "img/members/moses_big.jpg",
+            "Nickname": "Nickname : padre",
+            "DateOfBirth": "Date of : 24th March",
+            "Occupation": "Occupation : Catholic Priest"
+        },
+        {
+            "Name": "Animashun Azeez",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/AzeezAnimashun_big.png",
+            "Nickname": "Nickname : AZ",
+            "DateOfBirth": "Date of : 29th April",
+            "Occupation": "Occupation : Auto dealer"
+        },
+        {
+            "Name": "Akhanene Abu Syndiq",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/syndiq_big.jpg",
+            "Nickname": "Nickname : Mark Fish",
+            "DateOfBirth": "Date of : 29th April",
+            "Occupation": "Occupation : Manufacturing "
+        },
+        {
+            "Name": "Mutiu Temitope Orolu",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/Mutiu_big.jpg",
+            "Nickname": "Nickname : Mark Fish",
+            "DateOfBirth": "Date of : 13th Feb",
+            "Occupation": "Occupation : Business Man"
+        },
+        {
+            "Name": " Matthew Yusuf",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/MatoLight_big-min.jpg",
+            "Nickname": "Nickname : Matolight",
+            "DateOfBirth": "Date of : 28th July",
+            "Occupation": "Occupation : Film maker (light designer)"
+        },
+        {
+            "Name": "David Ekun",
+            "Location": "  United Kingdom",
+            "imgPath": "img/members/DavidEkun_Big-min.jpg",
+            "Nickname": "Nickname : Beast",
+            "DateOfBirth": "Date of : April 28th",
+            "Occupation": "Occupation : Investment Banker"
+        },
+        {
+            "Name": "Sunmola Olalekan Saheed",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/sunmolaSaheed_big.png",
+            "Nickname": "Nickname : Simple",
+            "DateOfBirth": "Date of : 4th January",
+            "Occupation": "Occupation : Legit Hustle/ Businessman"
+        },
+        {
+            "Name": "Oluwatoyin Duntoye",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/ToyinDuntoye_big.jpg",
+            "Nickname": "Nickname : TY",
+            "DateOfBirth": "Date of : July 27th",
+            "Occupation": "Occupation : Compu-Graphics Artist/Printing"
+        },
+        {
+            "Name": "Alaye Sunny Harry",
+            "Location": "  Abuja, Nigeria ",
+            "imgPath": "img/members/avatar.png",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 1st August",
+            "Occupation": "Occupation : Teacher/Educator"
+        },
+        {
+            "Name": "Odugbose Detola",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/avatar.png",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 19th September",
+            "Occupation": "Occupation : Tax administrator"
+        },
+        {
+            "Name": "Olushola Joseph Shoyege",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/OLUSHOLA-JOSEPH-SHOYEGE-big-min.jpg",
+            "Nickname": "Nickname :  Shobaba",
+            "DateOfBirth": "Date of : 15th March",
+            "Occupation": "Occupation : Business"
+        },
+        {
+            "Name": "Adeponle Patrick Kunle",
+            "Location": "  Port-Harcourt, Nigeria",
+            "imgPath": "img/members/Adepole_Patrick_big.jpg",
+            "Nickname": "Nickname : Cele",
+            "DateOfBirth": "Date of : 23rd June",
+            "Occupation": "Occupation : Salesman"
+        },
+        {
+            "Name": "Salvador Olatunbosun",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/Salvador Olatunbosun_big.jpg",
+            "Nickname": "Nickname : Commander",
+            "DateOfBirth": "Date of : 6th June ",
+            "Occupation": "Occupation : Business Man"
+        },
+        {
+            "Name": "Ademola Lanisa",
+            "Location": "  France",
+            "imgPath": "img/members/lanisa_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 2nd October",
+            "Occupation": "Occupation : Exploration Geophysicist"
+        },
+        {
+            "Name": "Vitalis Anoka M",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/VitalisAnokam_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 26th December",
+            "Occupation": "Occupation : Quantity Surveyor"
+        },
+        {
+            "Name": "Qudus Oshunade",
+            "Location": "  Maryland, USA ",
+            "imgPath": "img/members/QD_big-min.jpg",
+            "Nickname": "Nickname : QD",
+            "DateOfBirth": "Date of : 9th August ",
+            "Occupation": "Occupation : IT Administrator"
+        },
+        {
+            "Name": "Wale Alaba",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/WaleAlaba_big.jpg",
+            "Nickname": "Nickname : Greg's",
+            "DateOfBirth": "Date of :8th April",
+            "Occupation": "Occupation : Lawyer"
+        },
+        {
+            "Name": "Francis Ekwebelam",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/Francis Ekwebelam_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 2nd October",
+            "Occupation": "Occupation : IT consultant"
+        },
+        {
+            "Name": "Sarumi Mujib",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/SarumiMujib_big.jpg",
+            "Nickname": "Nickname : Scotchie / Emjay",
+            "DateOfBirth": "Date of : 13th September",
+            "Occupation": "Occupation : Civil Servant / Water Engineer"
+        },
+        {
+            "Name": "Dakobiri Shola",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/solaDakobi_big.jpg",
+            "Nickname": "Nickname : Solar",
+            "DateOfBirth": "Date of : 14th July",
+            "Occupation": "Occupation : Civil Servant"
+        },
+        {
+            "Name": "Omotosho Salau",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/Tosho_big.jpg",
+            "Nickname": "Nickname : Toshiba",
+            "DateOfBirth": "Date of : 9th May",
+            "Occupation": "Occupation : Tax Administrator"
+        },
+        {
+            "Name": "Olawale Grillo",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/waleGrillo_big.jpg",
+            "Nickname": "Nickname : AZ",
+            "DateOfBirth": "Date of : 15th October",
+            "Occupation": "Occupation : Real Estate"
+        },
+        {
+            "Name": "Douglas Agu",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/douglas_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 9th May",
+            "Occupation": "Occupation : Entrepreneur (Marketing Consultant)"
+        },
+        {
+            "Name": "Tari Jasper",
+            "Location": "  Maryland, USA ",
+            "imgPath": "img/members/TariJasper_big.jpg",
+            "Nickname": "Nickname : Tari J",
+            "DateOfBirth": "Date of : 4th June",
+            "Occupation": "Occupation : Entertainment and Sport"
+        },
+        {
+            "Name": "Tunde Tinubu",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/TundeTinubu_big.jpg",
+            "Nickname": "Nickname : ogboriko",
+            "DateOfBirth": "Date of :2nd March ",
+            "Occupation": "Occupation : Cateering Service"
+        },
+        {
+            "Name": "King Shina",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/KingShina_Big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 29th August",
+            "Occupation": "Occupation : Accountant"
+        },
+        {
+            "Name": "Adeboni Oladele",
+            "Location": "  Lagos, Nigeria",
+            "imgPath": "img/members/AdeboniOladele_big.jpg",
+            "Nickname": "Nickname : Sir J",
+            "DateOfBirth": "Date of : 4th March",
+            "Occupation": "Occupation : Fleet Management"
+        },
+        {
+            "Name": "Ohaegbu Patrick",
+            "Location": "   Lagos, Nigeria",
+            "imgPath": "img/members/Ohaegbu Patrick_big.jpg",
+            "Nickname": "Nickname : ",
+            "DateOfBirth": "Date of : 23rd July",
+            "Occupation": "Occupation : HSE Practitioner"
+        },
+
+        {
+            "Name": "Olasegun Ola-Dawodu SOD",
+            "Location": "   Lagos, Nigeria",
+            "imgPath": "img/members/olusegun_big.jpg",
+            "Nickname": "Nickname : Klinsman",
+            "DateOfBirth": "Date of : 19th August",
+            "Occupation": "Occupation : Insurance"
+        },
+        {
+            "Name": "Abimbola Oredugba",
+            "Location": "   Manchester, UK",
+            "imgPath": "img/members/Abimbola_Oredugba.JPG",
+            "Nickname": "Nickname : Orex",
+            "DateOfBirth": "Date of : 10th December",
+            "Occupation": "Occupation : "
         },
     ]
 
